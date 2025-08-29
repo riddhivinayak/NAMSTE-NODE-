5b473df361a5802b0f3006ff328af0084b442ff7
riddhi vinayak <riddhivinayak001@gmail.com>
2025-08-29 10:56:55 +0530

    basics of monogodb and mongoose(sechema,models)

--- config/datatbase.js
+++ config/datatbase.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+import pass from './password.js';
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://riddhivinayak001:5Cpn6L33o2sMKsmJ@mastetest1.lgurj.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://riddhivinayak001:"+pass+"@mastetest1.lgurj.mongodb.net/devTinder");
 };// writing a name infornt of the url connects to specifc database if not present then it creates a new one
  
 export default connectDB;
